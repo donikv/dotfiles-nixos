5264b1e000fa7b3b3d8c43287bb72c8a3ab943c0
donikv <donikv@gmail.com>
2024-02-05 18:20:38 +0100

    Adds android and dev files

--- common.nix
+++ common.nix
@@ -18,28 +18,7 @@
   maestral-gui
   maestral
   xournalpp
-  (vscode-with-extensions.override {
-    vscodeExtensions = with vscode-extensions; [
-      bbenoist.nix
-      ms-python.python
-      ms-vscode-remote.remote-ssh
-      vscodevim.vim
-      arrterian.nix-env-selector
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "remote-ssh-edit";
-        publisher = "ms-vscode-remote";
-        version = "0.47.2";
-        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-      }]
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "copilot";
-        publisher = "GitHub";
-        version = "1.46.6822";
-        sha256 = "sha256-L71mC0190ZubqNVliu7es4SDsBTGVokePpcNupABI8Q=";
-      }];
-     })
+  libsForQt5.okular
   ];
 
   services.printing.enable = true;
---
+++ plasma6.nix
@@ -0,0 +1,13 @@
+{pkgs ? import <nixpkgs> { }, ...}: {
+  
+  imports = [
+    <kde2nix/nixos.nix>
+  ];
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmawayland";
+}
