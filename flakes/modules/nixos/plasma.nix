ec83eb4e78519a9f23712034abbd9855d607beb1
donikv <donikv@gmail.com>
2024-06-03 15:28:55 +0200

    Revert "Revert "Adds plasma5 config""
    This reverts commit 45ed3b39478d09c9d6db0d27244ff0ab05243683.

---
+++ flakes/modules/nixos/plasma.nix
@@ -0,0 +1,9 @@
+{pkgs ? import <nixpkgs> { }, ...}: {
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  #services.xserver.displayManager.defaultSession = "plasmawayland";
+}
