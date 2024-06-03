e1098ea2770445810b9c72d4bd234f53e5e0393a
donikv <donikv@gmail.com>
2024-06-03 15:16:29 +0200

    Revert "Adds initial plasma manager config"
    This reverts commit 334b4fefd1b0a51360ea737561a0cc1a58e9d998.

--- flakes/modules/home-manager/plasma-config-module.nix
+++ flakes/modules/home-manager/plasma-config-module.nix
@@ -1,63 +1,14 @@
-{pkgs ? import <nixpkgs> { }, plasma-manager, ...}: {
+{pkgs ? import <nixpkgs> { }, ...}: {
 
-  # qt = {
-  #   enable = true;
-  #   platformTheme = "qtct";
-  #   style.name = "kvantum";
-  # };
-  
-  # xdg.configFile = {
-  #   "Kvantum/ArcDark".source = "${pkgs.arc-kde-theme}/share/Kvantum/ArcDark";
-  #   "Kvantum/kvantum.kvconfig".text = "[General]\ntheme=ArcDark";
-  # };
-  # imports = [
-  #   plasma-manager.homeManagerModules.plasma-manager
-  # ];
-
-  programs.plasma = {
+  qt = {
     enable = true;
-    workspace = {
-      clickItemTo = "select";
-      lookAndFeel = "org.kde.breezetwilight.desktop";
-      cursorTheme = "Bibata-Modern-Ice";
-      iconTheme = "Papirus";
-      wallpaper = "${pkgs.libsForQt5.plasma-workspace-wallpapers}/share/wallpapers/Patak/contents/images/1080x1920.png";
-    };
-    panels = [
-      # Windows-like panel at the bottom
-      {
-        location = "bottom";
-        widgets = [
-          "org.kde.plasma.kickoff"
-          # We can also configure the widgets. For example if you want to pin
-          # konsole and dolphin to the task-launcher the following widget will
-          # have that.
-          {
-            name = "org.kde.plasma.icontasks";
-            config = {
-              General.launchers = [
-                "applications:org.kde.dolphin.desktop"
-                "applications:org.kde.konsole.desktop"
-                "applications:firefox.desktop"
-                "applications:code.desktop"
-                "applications:spotify.desktop"
-              ];
-            };
-          }
-          "org.kde.plasma.marginsseparator"
-          "org.kde.plasma.systemtray"
-          "org.kde.plasma.digitalclock"
-        ];
-      }
-      # Global menu at the top
-      {
-        location = "top";
-        height = 26;
-        widgets = [
-          "org.kde.plasma.appmenu"
-        ];
-      }
-    ];
+    platformTheme = "qtct";
+    style.name = "kvantum";
+  };
+  
+  xdg.configFile = {
+    "Kvantum/ArcDark".source = "${pkgs.arc-kde-theme}/share/Kvantum/ArcDark";
+    "Kvantum/kvantum.kvconfig".text = "[General]\ntheme=ArcDark";
   };
 }
 
