38cd67f738c2546b2e88b7b0ad3f3a9c3d1f8594
donikv <donikv@gmail.com>
2024-06-03 15:27:56 +0200

    Revert "Update to plasma 6"
    This reverts commit bdcf81de727cca5acf23558803c28c9d17743432.

--- flakes/modules/nixos/plasma.nix
+++ flakes/modules/nixos/plasma.nix
@@ -3,8 +3,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
   #services.xserver.displayManager.defaultSession = "plasmawayland";
 
   environment.systemPackages = with pkgs; [
