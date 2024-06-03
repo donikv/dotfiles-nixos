c6416a8f662c2ace47df73aae9174f53a51dbed4
donikv <donikv@gmail.com>
2024-06-03 15:27:56 +0200

    Revert "Revert "Update to plasma 6""
    This reverts commit 62a67df4d1316f9665284c09de1cd9e10896f112.

--- plasma.nix
+++ plasma.nix
@@ -3,7 +3,7 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
   #services.xserver.displayManager.defaultSession = "plasmawayland";
 }
