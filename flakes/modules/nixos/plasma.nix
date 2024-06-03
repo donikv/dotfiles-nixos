bdcf81de727cca5acf23558803c28c9d17743432
donikv <donikv@gmail.com>
2024-06-03 10:26:12 +0200

    Update to plasma 6

--- flakes/modules/nixos/plasma.nix
+++ flakes/modules/nixos/plasma.nix
@@ -3,8 +3,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
   #services.xserver.displayManager.defaultSession = "plasmawayland";
 
   environment.systemPackages = with pkgs; [
