bf994bcd934206b6e2d4ef56a56bd7d0572c12a8
donikv <donikv@gmail.com>
2024-06-03 15:27:57 +0200

    Revert "Adds plasma5 config"
    This reverts commit d9f8f04eb1a5244ca051644e346700dcc6a7df19.

--- flakes/nixos/configuration.nix
+++ flakes/nixos/configuration.nix
@@ -189,17 +189,6 @@
       theme = "robbyrussell";
     };
   };
-  system.autoUpgrade = {
-    enable = true;
-    flake = inputs.self.outPath;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "-L" # print build logs
-    ];
-    dates = "02:00";
-    randomizedDelaySec = "45min";
-  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
