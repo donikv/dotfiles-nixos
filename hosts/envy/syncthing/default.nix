05031f3db8831d0f3c6c5722ac6a24a9dd67531f
donikv <donikv@gmail.com>
2024-07-17 15:36:40 +0200

    Adds new syncthing config

--- hosts/envy/syncthing/default.nix
+++ hosts/envy/syncthing/default.nix
@@ -10,11 +10,12 @@
       settings = {
         devices = {
           "S24Ultra" = { id = "ZXEBA6R-M7VP3WG-7PMK2E5-KPAHRTE-566OJWV-2W7LFNV-2ZM2LFM-OMP4EQX"; };
+          "Lithium" = {id = "I234VTO-FX44ODQ-GUTWWMR-QEVMSJI-VMHJOOP-7YQLMJ5-Z7K7ZQG-N6ADZAR"; }; 
         };
         folders = {
           "Obsidian" = {         # Name of folder in Syncthing, also the folder ID
             path = "/home/donik/Documents/Obsidian";    # Which folder to add to Syncthing
-            devices = [ "S24Ultra" ] ;      # Which devices to share the folder with
+            devices = [ "S24Ultra" "Lithium" ] ;      # Which devices to share the folder with
           };
         };
       };
