b657f69ce3fb12029fbbbaa3f64c6cebad39fd82
Donik <donikv@gmail.com>
2025-01-30 11:55:51 +0100

    Changes pip

--- hosts/zver/default.nix
+++ hosts/zver/default.nix
@@ -25,8 +25,9 @@
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     vim
     git
-    python312Full
-    python312Packages.pip
+    (python312Full.withPackages(ps: with ps; [
+      ps.pip
+    ]))
   ];
   
   #Link python for ansible backwards compatibility with ubuntu machines
