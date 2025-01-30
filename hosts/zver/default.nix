a65b136b574c6149bbb74b456d2e09a48b66ac2c
Donik <donikv@gmail.com>
2025-01-30 11:28:56 +0100

    Adds python and pip

--- hosts/zver/default.nix
+++ hosts/zver/default.nix
@@ -23,9 +23,10 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     vim
     git
+    python312Full
+    python312Packages.pip
   ];
   
   xdg.portal = {
