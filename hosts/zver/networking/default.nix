64e21639bd86d9a0879b48ed7b01adcb0de4fcb3
Donik <donikv@gmail.com>
2025-06-14 12:47:41 +0200

    Updates a to insert

--- hosts/zver/networking/default.nix
+++ hosts/zver/networking/default.nix
@@ -20,15 +20,15 @@
     iptables -P OUTPUT ACCEPT
 
     # Allow loopback
-    iptables -A INPUT 1 -i lo -j ACCEPT
+    iptables -I INPUT 1 -i lo -j ACCEPT
 
     # Allow established connections
-    iptables -A INPUT 2 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables -I INPUT 2 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
     # Allow traffic from specified IPs and subnet
-    iptables -A INPUT 3 -s 10.0.0.0/8 -j ACCEPT
-    iptables -A INPUT 4 -s 161.53.78.45 -j ACCEPT
-    iptables -A INPUT 5 -s 161.53.78.46 -j ACCEPT
+    iptables -I INPUT 3 -s 10.0.0.0/8 -j ACCEPT
+    iptables -I INPUT 4 -s 161.53.78.45 -j ACCEPT
+    iptables -I INPUT 5 -s 161.53.78.46 -j ACCEPT
     iptables -I INPUT 6 -j DROP
   '';
 
