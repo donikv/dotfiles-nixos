bb9e5ce20f424722813ac8aff386b99d124c8d75
Donik <donikv@gmail.com>
2025-06-14 12:38:50 +0200

    Updates firewall

--- hosts/zver/networking/default.nix
+++ hosts/zver/networking/default.nix
@@ -6,40 +6,33 @@
   ];
   
   networking.firewall = { 
-    enable = false;
+    enable = true;
+    allowPing = true;
+    logRefusedConnections = false;
   }; 
-  #networking.firewall.enable = true;
-  # networking.firewall.allowPing = true;  # adjust as needed
-  # networking.firewall.logRefusedConnections = false;  # adjust as needed
+  
+  networking.firewall.extraCommands = ''
+    iptables -F INPUT
 
-  # networking.firewall.extraCommands = ''
-  #   iptables -P INPUT DROP
-  #   iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-  #   iptables -A INPUT -i lo -j ACCEPT
-  #   iptables -A INPUT -s 10.0.0.0/8 -j ACCEPT
-  #   iptables -A INPUT -s 161.53.78.45 -j ACCEPT
-  #   iptables -A INPUT -s 161.53.78.46 -j ACCEPT
-  # '';
-
-  # networking.firewall.extraStopCommands = ''
-  #   iptables -F
-  #   iptables -P INPUT ACCEPT
-  # '';
-
-  boot.extraCommands = ''
-    iptables -F
+    # Drop by default
     iptables -P INPUT DROP
     iptables -P FORWARD DROP
     iptables -P OUTPUT ACCEPT
 
-    iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    # Allow loopback
     iptables -A INPUT -i lo -j ACCEPT
+
+    # Allow established connections
+    iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+    # Allow traffic from specified IPs and subnet
     iptables -A INPUT -s 10.0.0.0/8 -j ACCEPT
     iptables -A INPUT -s 161.53.78.45 -j ACCEPT
     iptables -A INPUT -s 161.53.78.46 -j ACCEPT
   '';
 
-  boot.postBootCommands = ''
-    echo "Custom iptables rules applied"
+  networking.firewall.extraStopCommands = ''
+    iptables -F INPUT
+    iptables -P INPUT ACCEPT
   '';
 }
