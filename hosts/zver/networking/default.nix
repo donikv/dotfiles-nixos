c05332c609ae09ee2d0d4071ae7c3501cb3b9b95
Donik <donikv@gmail.com>
2025-06-14 12:12:34 +0200

    Adds firewall config for vpn

---
+++ hosts/zver/networking/default.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+{
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = false;  # adjust as needed
+  networking.firewall.logRefusedConnections = false;  # adjust as needed
+
+  networking.firewall.extraCommands = ''
+    iptables -P INPUT DROP
+    iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables -A INPUT -i lo -j ACCEPT
+    iptables -A INPUT -s 10.0.0.0/8 -j ACCEPT
+    iptables -A INPUT -s 161.53.78.45 -j ACCEPT
+    iptables -A INPUT -s 161.53.78.46 -j ACCEPT
+  '';
+
+  networking.firewall.extraStopCommands = ''
+    iptables -F
+    iptables -P INPUT ACCEPT
+  '';
+}
