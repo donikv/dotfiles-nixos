620341c5f718cb4f0602f382212552e84d0f7fb0
Donik <donikv@gmail.com>
2025-07-17 14:47:17 +0200

    Adds zver4 hardware config

--- hosts/zver4/hardware-configuration.nix
+++ hosts/zver4/hardware-configuration.nix
@@ -8,29 +8,25 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/227afb90-79d1-4a0c-b735-21baf58b9cde";
+    { device = "/dev/disk/by-uuid/809984d8-c2e2-47f5-a5f2-ade9dd238caf";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/294E-E8F4";
+    { device = "/dev/disk/by-uuid/C876-D10E";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/6af4ed76-ee8d-4449-a450-7a1b088ff75d"; }
+    [ { device = "/dev/disk/by-uuid/f27d4498-2736-464e-9c07-20b48a0ec6d6"; }
     ];
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/e5ec191a-1d7e-11e9-aeaf-0492265affe9";
-      fsType = "ext4";
-    };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
