7d8f7eacbc7642aa525937668dcbfbb134633dee
Donik <donikv@gmail.com>
2025-11-25 10:57:14 +0100

    Iptable allow FER

--- hosts/zver/networking/default.nix
+++ hosts/zver/networking/default.nix
@@ -27,11 +27,12 @@
 
     # Allow traffic from specified IPs and subnet
     iptables -I INPUT 3 -s 10.0.0.0/8 -j ACCEPT # Local network
-    iptables -I INPUT 4 -s 161.53.78.45 -j ACCEPT #FER VPN
-    iptables -I INPUT 5 -s 161.53.78.46 -j ACCEPT
-    iptables -I INPUT 6 -s 161.53.64.207 -j ACCEPT #ZVER 0
-    iptables -I INPUT 7 -s 161.53.64.7 -j ACCEPT #ZVER Router
-    iptables -I INPUT 8 -j DROP
+    iptables -I INPUT 4 -s 161.53.0.0/16 -j ACCEPT #FER network
+    iptables -I INPUT 5 -s 161.53.78.45 -j ACCEPT #FER VPN
+    iptables -I INPUT 6 -s 161.53.78.46 -j ACCEPT
+    iptables -I INPUT 7 -s 161.53.64.207 -j ACCEPT #ZVER 0
+    iptables -I INPUT 8 -s 161.53.64.7 -j ACCEPT #ZVER Router
+    iptables -I INPUT 9 -j DROP
   '';
 
   networking.firewall.extraStopCommands = ''
