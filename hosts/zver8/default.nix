b056b5fbcdd1fd79e291436a8b97fb8b52d2272c
Donik <donikv@gmail.com>
2026-05-16 12:08:55 +0200

    Adds inital config for zver7 and zver8.

---
+++ hosts/zver8/default.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+{
+  imports = [
+    ../zver
+  ];
+  
+  systemd.services.NetworkManager-wait-online.enable = false;
+  users.ldap.extraConfig = ''
+      pam_filter memberOf=cn=zver8,ou=Machines,dc=ipg,dc=com
+  '';
+  networking = {
+    interfaces.eno1 = {
+      ipv4.addresses = [{
+        address = "161.53.64.196";
+        prefixLength = 24;
+      }
+      {
+        address = "10.53.64.196";
+        prefixLength = 16;
+      }
+      ];
+    };
+    defaultGateway = {
+      address = "10.53.0.1";
+      interface = "eno1";
+    };
+  };
+ networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+ networking.hosts = {
+    "127.0.1.1" = ["zver8"];
+    "127.0.0.1" = ["localhost"];
+ #   "192.0.2.1" = ["mail.example.com" "imap.example.com"];
+ };
+}
