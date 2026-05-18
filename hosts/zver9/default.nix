cbea3f8042f9af93f90722b786bc6760573206c0
Donik <donikv@gmail.com>
2026-05-18 11:45:17 +0200

    Adds initial hosts 2, 9, 10

---
+++ hosts/zver9/default.nix
@@ -0,0 +1,34 @@
+{ config, lib, pkgs, ... }:
+{
+  imports = [
+    ../zver
+  ];
+  
+  systemd.services.NetworkManager-wait-online.enable = false;
+  users.ldap.extraConfig = ''
+      pam_filter memberOf=cn=zver9,ou=Machines,dc=ipg,dc=com
+  '';
+  networking = {
+    interfaces.enp69s0 = {
+      ipv4.addresses = [{
+        address = "161.53.64.187";
+        prefixLength = 24;
+      }
+      {
+        address = "10.53.64.187";
+        prefixLength = 16;
+      }
+      ];
+    };
+    defaultGateway = {
+      address = "10.53.0.1";
+      interface = "enp69s0";
+    };
+  };
+ networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+ networking.hosts = {
+    "127.0.1.1" = ["zver9"];
+    "127.0.0.1" = ["localhost"];
+ #   "192.0.2.1" = ["mail.example.com" "imap.example.com"];
+ };
+}
