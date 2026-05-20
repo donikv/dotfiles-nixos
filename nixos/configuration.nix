34b46554645064d3984c49514be4f045062ae804
Donik Vršnak <donikv@gmail.com>
2026-05-20 22:31:07 +0200

    Turns off auto updates
    Turns off auto updates becuase they could crash docker

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -115,15 +115,15 @@
   };
 
   system.autoUpgrade = {
-    enable = true;
-    flake = inputs.self.outPath;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "-L" # print build logs
-    ];
-    dates = "02:00";
-    randomizedDelaySec = "45min";
+    enable = false;
+    #flake = inputs.self.outPath;
+    #flags = [
+    #  "--update-input"
+    #  "nixpkgs"
+    #  "-L" # print build logs
+    #];
+    #dates = "02:00";
+    #randomizedDelaySec = "45min";
   };
 
   nix.gc = {
